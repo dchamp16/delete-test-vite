45515064f63b1a9ca933c7b1be54b80eab03178b
Peter Ramos <86265240+dchamp16@users.noreply.github.com>
2024-01-28 20:56:28 +0000

    created a form

---
+++ this-is-just-a-test-delete/src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import FormWebsite from "./components/FormWebsite";
+
+function App() {
+  return (
+    <>
+      <FormWebsite />
+    </>
+  );
+}
+
+export default App;
---
+++ this-is-just-a-test-delete/src/components/FormWebsite.jsx
@@ -0,0 +1,65 @@
+import React from "react";
+import { useState } from "react";
+import "./components-style/FormWebsite.css";
+
+const FormWebsite = () => {
+  let [email, setEmail] = useState("peter");
+
+  function handleSetEmail() {
+    const customerEmail = document.getElementById("exampleInputEmail1").value;
+    setEmail(customerEmail);
+  }
+
+  return (
+    <>
+      <h1>{email}</h1>
+      <div className="container m-5">
+        <form className="p-5 form-border">
+          <div className="mb-3">
+            <label for="exampleInputEmail1" className="form-label">
+              Email address
+            </label>
+            <input
+              type="email"
+              className="form-control"
+              id="exampleInputEmail1"
+              aria-describedby="emailHelp"
+            ></input>
+            <div id="emailHelp" className="form-text">
+              We'll never share your email with anyone else.
+            </div>
+          </div>
+          <div className="mb-3">
+            <label for="exampleInputPassword1" className="form-label">
+              Password
+            </label>
+            <input
+              type="password"
+              className="form-control"
+              id="exampleInputPassword1"
+            ></input>
+          </div>
+          <div className="mb-3 form-check">
+            <input
+              type="checkbox"
+              className="form-check-input"
+              id="exampleCheck1"
+            ></input>
+            <label className="form-check-label" for="exampleCheck1">
+              Check me out
+            </label>
+          </div>
+          <button
+            type="submit"
+            className="btn btn-primary"
+            onClick={handleSetEmail}
+          >
+            Submit
+          </button>
+        </form>
+      </div>
+    </>
+  );
+};
+
+export default FormWebsite;
